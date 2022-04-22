112f7a364d5475564458ae700cad993bcee680e6
Hudson <hudsonchristopherkelly@gmail.com>
2022-04-21 21:43:18 -0500

    Update example Recaptcha

--- examples/studio-kit-demo/config.ts
+++ examples/studio-kit-demo/config.ts
@@ -5,11 +5,13 @@
 type Config = {
   env: 'stage' | 'prod'
   logLevel: 'Debug' | 'Info' | 'Warn' | 'Error'
+  recaptchaKey: string
 }
 
 const LOCAL_ENV: Config['env'] = 'stage'
 
 export default {
-  env: LOCAL_ENV,
+  env: location.hostname === 'live.api.stream' ? 'prod' : LOCAL_ENV,
   logLevel: 'Debug',
+  recaptchaKey: '6Lc0HIUfAAAAAIdsyq7vB_3c3skiVvltzdUTCUSx',
 } as Config
