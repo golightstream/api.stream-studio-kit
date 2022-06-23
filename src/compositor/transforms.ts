6dea8a62bbd332e7066fe61c84213ae44fd6cda7
maddygoround <mahendra@golightstream.com>
2022-06-23 14:12:17 +0530

    Changes for Event triggers on videoended and videotimeupdate

--- src/compositor/transforms.ts
+++ src/compositor/transforms.ts
@@ -1,3 +1,7 @@
+/* ---------------------------------------------------------------------------------------------
+ * Copyright (c) Infiniscene, Inc. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * -------------------------------------------------------------------------------------------- */
 import type { PropsDefinition, SceneNode } from './compositor'
 import type { Source } from './sources'
 
