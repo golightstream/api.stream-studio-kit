3f624c253a50cb294b5ec7826a92ef80124d605c
maddygoround <mahendra@golightstream.com>
2022-06-24 15:43:14 +0530

    Overlays and clips added

--- examples/studio-kit-demo/src/shared/participant.tsx
+++ examples/studio-kit-demo/src/shared/participant.tsx
@@ -10,29 +10,9 @@ import Style from './shared.module.css'
 const { Room } = Helpers
 const { useStudio } = Helpers.React
 
-const overlays = [
-  {
-    id: '123',
-    url: 'https://www.pngmart.com/files/12/Twitch-Stream-Overlay-PNG-Transparent-Picture.png',
-  },
-  {
-    id: '124',
-    url: 'https://www.pngmart.com/files/12/Stream-Overlay-Transparent-PNG.png',
-  },
-]
 
-const videooverlays = [
-  {
-    id: '125',
-    url: 'https://assets.mixkit.co/videos/preview/mixkit-stars-in-space-1610-large.mp4',
-  },
-  {
-    id: '126',
-    url: 'https://assets.mixkit.co/videos/preview/mixkit-curvy-road-on-a-tree-covered-hill-41537-large.mp4',
-  },
-]
 export const Participants = () => {
-  const { room, projectCommands } = useStudio()
+  const { room, projectCommands, studio } = useStudio()
   const { isHost } = useContext(AppContext)
   const [participants, setParticipants] = useState<SDK.Participant[]>([])
 
@@ -45,6 +25,7 @@ export const Participants = () => {
       if (isHost) projectCommands.pruneParticipants()
     })
   }, [room])
+
 
   return (
     <div className={Style.column}>
@@ -228,7 +209,7 @@ const HostControls = ({
   type,
 }: ParticipantProps & { type: 'screen' | 'camera' }) => {
   const { id } = participant
-  const { projectCommands } = useStudio()
+  const { projectCommands, studio  } = useStudio()
 
   // Get the initial props in case the participant is on stream
   const projectParticipant = useMemo(
@@ -240,9 +221,7 @@ const HostControls = ({
   const [isMuted, setIsMuted] = useState(projectParticipant?.isMuted ?? false)
   const [volume, setVolume] = useState(projectParticipant?.volume ?? 1)
   const [isShowcase, setIsShowcase] = useState(false)
-  const [selectedVideo, setSelectedVideo] = useState(null)
-  const [selectedImage, setSelectedImage] = useState(null)
-  const [projectedLoaded, setProjectedLoaded] = useState(false)
+
   // Monitor whether the participant has been removed from the stream
   //  from some other means (e.g. dragged off canvas by host)
   useEffect(() => {
@@ -266,12 +245,7 @@ const HostControls = ({
     [],
   )
 
-  useEffect(() => {
-    const videoOverlayId = projectCommands.getVideoOverlay()
-    if (typeof videoOverlayId === 'string') {
-      projectCommands.playOverlay(videoOverlayId)
-    }
-  }, [])
+
 
   return (
     <div
@@ -362,52 +336,6 @@ const HostControls = ({
           </>
         )}
       </span>
-      <div>
-        <span>
-          Overlays
-          <ul>
-            {overlays.map((overlay) => (
-              <li
-                key={overlay.id}
-                onClick={() => {
-                  if (selectedImage !== overlay.id) {
-                    setSelectedImage(overlay.id)
-                    projectCommands.addImageOverlay(overlay.id, overlay.url)
-                  } else {
-                    projectCommands.removeImageOverlay(selectedImage)
-                    setSelectedImage(null)
-                  }
-                }}
-              >
-                <img width="40px" height="50px" src={overlay.url} />
-              </li>
-            ))}
-          </ul>
-        </span>
-      </div>
-      <div>
-        <span>
-          Video clips
-          <ul>
-            {videooverlays.map((overlay) => (
-              <li
-                key={overlay.id}
-                onClick={() => {
-                  if (selectedVideo !== overlay.id) {
-                    setSelectedVideo(overlay.id)
-                    projectCommands.addVideoOverlay(overlay.id, overlay.url)
-                  } else {
-                    projectCommands.removeVideoOverlay(selectedVideo)
-                    setSelectedVideo(null)
-                  }
-                }}
-              >
-                <video width="40px" height="50px" src={overlay.url} />
-              </li>
-            ))}
-          </ul>
-        </span>
-      </div>
     </div>
   )
 }
