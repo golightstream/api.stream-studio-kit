f7ae609cd6bf5bcdd992089e357efb18c84a04f1
maddygoround <mahendra@golightstream.com>
2022-08-30 00:40:13 +0530

    Remove unwanted iframe properties

--- src/core/types.ts
+++ src/core/types.ts
@@ -828,48 +828,14 @@ export interface IframeProps {
   display?: 'block' | 'none' | 'inline'
   height?: string
   width?: string
-  loading?: 'auto' | 'eager' | 'lazy'
-  target?: string
-  importance?: 'auto' | 'high' | 'low'
   overflow?: string
+  target?: string
   styles?: object
   name?: string
-  allowpaymentrequest?: string
-  referrerPolicy?:
-    | 'no-referrer'
-    | 'no-referrer-when-downgrade'
-    | 'origin'
-    | 'origin-when-cross-origin'
-    | 'same-origin'
-    | 'strict-origin'
-    | 'strict-origin-when-cross-origin'
-    | 'unsafe-url'
   onLoad?: () => void
-  onMouseOver?: () => void
-  onMouseOut?: () => void
   frameBorder?: number
-  scrolling?: 'auto' | 'yes' | 'no'
   id?: string
-  ariaHidden?: boolean
-  ariaLabel?: string
-  ariaLabelledby?: string
-  sandbox?:
-    | 'allow-forms'
-    | 'allow-modals'
-    | 'allow-orientation-lock'
-    | 'allow-pointer-lock'
-    | 'allow-popups'
-    | 'allow-popups-to-escape-sandbox'
-    | 'allow-presentation'
-    | 'allow-same-origin'
-    | 'allow-scripts'
-    | 'allow-storage-access-by-user-activation'
-    | 'allow-top-navigation'
-    | 'allow-top-navigation-by-user-activation'
-  allow?: string
   className?: string
-  title?: string
   children?: React.ReactNode
   iframeRef?: React.Ref<HTMLIFrameElement>
-  allowtransparency?: string
 }
