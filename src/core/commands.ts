2de3462fb32fde6bad5d2145404c01af46193c58
maddygoround <mahendra@golightstream.com>
2022-09-01 17:00:53 +0530

    remove metadata from updatingw hile changing rendering properties

--- src/core/commands.ts
+++ src/core/commands.ts
@@ -264,7 +264,7 @@ export const setActiveProject = async (payload: {
           framerate: 30,
         },
       },
-      updateMask: ["rendering","metadata"]
+      updateMask: ["rendering"]
     })
   }
 
