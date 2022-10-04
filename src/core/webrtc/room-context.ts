c9cdcd157a58ab9074c610404d00be4c36088ea0
maddygoround <mahendra@golightstream.com>
2022-10-04 23:34:37 +0530

    update participants info on metadata changes

--- src/core/webrtc/room-context.ts
+++ src/core/webrtc/room-context.ts
@@ -425,6 +425,10 @@ export class RoomContext implements LSRoomContext {
       RoomEvent.LocalTrackUnpublished,
       this._updateParticipants,
     )
+    this.subscribeToRoomEvent(
+      RoomEvent.ParticipantMetadataChanged,
+      this._updateParticipants
+    )
     this.subscribeToRoomEvent(RoomEvent.Disconnected, () => {
       this.livekitRoom.removeAllListeners()
       this.livekitRoom = null
