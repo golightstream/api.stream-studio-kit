44efd0a542b96f463c8bf1bad253645c81abfaae
maddygoround <mahendra@golightstream.com>
2022-11-06 13:33:43 +0530

    pass mediastreamtrack object instead whole track

--- src/core/sources/WebRTC.ts
+++ src/core/sources/WebRTC.ts
@@ -71,7 +71,7 @@ export const RoomParticipant = {
                     participant?.meta[track.id]?.displayName ||
                     'External Track',
                   mirrored: participant?.meta[track.id]?.isMirrored,
-                  ...(microphoneTrack) && {microphone : microphoneTrack},
+                  ...(microphoneTrack) && {microphone : microphoneTrack?.mediaStreamTrack},
                   external: track?.isExternal,
                 })
               }
