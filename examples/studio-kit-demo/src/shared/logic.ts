ee0b537bd637779283696ba459f66e764d37f1a1
Hudson <hud.kly@gmail.com>
2022-11-17 02:22:02 -0600

    Update missing headers

--- examples/studio-kit-demo/src/shared/logic.ts
+++ examples/studio-kit-demo/src/shared/logic.ts
@@ -1,3 +1,7 @@
+/* ---------------------------------------------------------------------------------------------
+ * Copyright (c) Infiniscene, Inc. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * -------------------------------------------------------------------------------------------- */
 // Ensure an item is iterable as an array
 export const asArray = <T>(x: T | T[]): T[] => {
   return Array.isArray(x) ? (x as T[]) : [x as T]
--- examples/studio-kit-demo/src/shared/props.tsx
+++ examples/studio-kit-demo/src/shared/props.tsx
@@ -1,3 +1,7 @@
+/* ---------------------------------------------------------------------------------------------
+ * Copyright (c) Infiniscene, Inc. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * -------------------------------------------------------------------------------------------- */
 import React from 'react'
 import { Compositor } from '@api.stream/studio-kit'
 import { Column } from '../ui/Box'
