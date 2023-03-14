7a6e86d19d35e3cd66767b56e5134005443cbb7a
maddygoround <mahendra@golightstream.com>
2023-03-14 23:53:06 +0530

    chore: update studio-kit demo examples

---
+++ examples/studio-kit-demo/src/host/components/tile.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { cssRaw, style } from 'typestyle'
+
+interface MediaHeaderProps {
+  title: string
+}
+
+export const MediaHeader = (props: MediaHeaderProps) => {
+  cssRaw(`.media---subhead {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 4px 0px 0px;
+}
+.text-1 {
+  text-align: left;
+  vertical-align: middle;
+  font-size: 12px;
+  font-family: Kievit Pro;
+  letter-spacing: 8%;
+  line-height: 120.00000476837158%;
+  color: #bababa;
+}
+.icon-toggle {
+  height: 24px;
+  width: 24px;
+  background-color: #ffffff;
+}
+..base-----icon-toggle {
+  height: 24px;
+  width: 24px;
+  background-color: #ffffff;
+}
+.img-2 {
+  height: 24px;
+  width: 24px;
+  background-color: #ffffff;
+}`)
+  return (
+    <div className="media---subhead">
+      <p className="text-1">{props.title}</p>
+    </div>
+  )
+}
+
+export interface MediadataProps {
+  id: string
+  url: string
+  thumbnail: string
+}
+
+interface MediaRowProps {
+  data: MediadataProps[]
+  type?: string
+  handleClick: (item: MediadataProps, type: string) => void
+  selected: boolean
+}
+function isValidImageURL(str:string) {
+  if (typeof str !== 'string') return false
+  return !!str.match(/\w+\.(jpg|jpeg|gif|png|tiff|bmp)$/gi)
+}
+export const MediaRow = (props: MediaRowProps) => {
+  const mediaRow = style({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: '4px',
+  })
+  const mediaThumbnail = style({
+    height: '68px',
+    width: '120px',
+    backgroundColor: '#ffffff',
+  })
+  const baseMediaThumbnail = style({
+    borderRadius: '8px',
+    height: '68px',
+    width: '120px',
+  })
+
+  const frame = style({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  })
+
+  return (
+    <div className={mediaRow}>
+      {props.data.map((item) => (
+        <img
+          aria-disabled={!props.selected}
+          key={item.id}
+          className={mediaThumbnail}
+          src={item.thumbnail}
+          onClick={() => props.handleClick(item, props.type ? props.type :  isValidImageURL(item.url) ? 'image' : 'video' )}
+        />
+      ))}
+    </div>
+  )
+}
---
+++ examples/studio-kit-demo/src/host/data.ts
@@ -0,0 +1,77 @@
+export const overlays = [
+  {
+    id: '123',
+    url: 'https://www.pngmart.com/files/12/Twitch-Stream-Overlay-PNG-Transparent-Picture.png',
+    thumbnail:
+      'https://www.pngmart.com/files/12/Twitch-Stream-Overlay-PNG-Transparent-Picture.png',
+  },
+  {
+    id: '124',
+    url: 'https://www.pngmart.com/files/12/Stream-Overlay-Transparent-PNG.png',
+    thumbnail:
+      'https://www.pngmart.com/files/12/Stream-Overlay-Transparent-PNG.png',
+  },
+  {
+    id: '125',
+    url: 'https://www.pngmart.com/files/12/Stream-Overlay-Transparent-PNG.png',
+    thumbnail:
+      'https://www.pngmart.com/files/12/Stream-Overlay-Transparent-PNG.png',
+  },
+]
+
+  export const videooverlays = [
+    {
+      id: '125',
+      url: 'https://assets.mixkit.co/videos/preview/mixkit-stars-in-space-1610-large.mp4',
+      thumbnail:
+        'https://i.ibb.co/Zd0RfMM/Screenshot-from-2023-03-13-22-41-14.png',
+    },
+    {
+      id: '126',
+      url: 'https://assets.mixkit.co/videos/preview/mixkit-curvy-road-on-a-tree-covered-hill-41537-large.mp4',
+      thumbnail:
+        'https://i.ibb.co/YR4fdLt/Screenshot-from-2023-03-13-22-42-57.png',
+    },
+    {
+      id: '127',
+      url: 'https://assets.mixkit.co/videos/preview/mixkit-curvy-road-on-a-tree-covered-hill-41537-large.mp4',
+      thumbnail:
+        'https://i.ibb.co/YR4fdLt/Screenshot-from-2023-03-13-22-42-57.png',
+    },
+  ]
+
+    export const backgrounds = [
+      {
+        id: '125',
+        url: 'https://assets.mixkit.co/videos/preview/mixkit-stars-in-space-1610-large.mp4',
+        thumbnail:
+          'https://i.ibb.co/Zd0RfMM/Screenshot-from-2023-03-13-22-41-14.png',
+      },
+      {
+        id: '126',
+        url: 'https://assets.mixkit.co/videos/preview/mixkit-curvy-road-on-a-tree-covered-hill-41537-large.mp4',
+        thumbnail:
+          'https://i.ibb.co/YR4fdLt/Screenshot-from-2023-03-13-22-42-57.png',
+      },
+      {
+        id: '127',
+        url: 'https://www.pngmart.com/files/12/Stream-Overlay-Transparent-PNG.png',
+        thumbnail:
+          'https://www.pngmart.com/files/12/Stream-Overlay-Transparent-PNG.png',
+      },
+    ]
+
+     export const logos = [
+        {
+          id: '128',
+          url: 'https://www.pngmart.com/files/12/Twitch-Stream-Overlay-PNG-Transparent-Picture.png',
+          thumbnail:
+            'https://www.pngmart.com/files/12/Twitch-Stream-Overlay-PNG-Transparent-Picture.png',
+        },
+        {
+          id: '129',
+          url: 'https://www.pngmart.com/files/12/Stream-Overlay-Transparent-PNG.png',
+          thumbnail:
+            'https://www.pngmart.com/files/12/Stream-Overlay-Transparent-PNG.png',
+        },
+      ]
