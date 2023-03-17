9fae661701b3dd082c6f1ea98b5465442c355324
maddygoround <mahendra@golightstream.com>
2023-03-17 23:23:06 +0530

    test: testing old livekitclient

--- examples/studio-kit-demo/src/host/data.ts
+++ examples/studio-kit-demo/src/host/data.ts
@@ -1,3 +1,7 @@
+/* ---------------------------------------------------------------------------------------------
+ * Copyright (c) Infiniscene, Inc. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * -------------------------------------------------------------------------------------------- */
 export const overlays = [
   {
     id: '123',
