ba009df05a4c523a89c3815480f9825b9358ae56
hudbuddy <hud.kly@gmail.com>
2024-09-13 01:04:05 -0400

    Fix iFrame dimensions and transparency

--- src/core/transforms/Overlay.tsx
+++ src/core/transforms/Overlay.tsx
@@ -41,193 +41,202 @@ export const Overlay = {
     const role = getProject(CoreContext.state.activeProjectId).role
     let interval: NodeJS.Timeout
 
-    const IFrame = React.memo(({
-      source,
-      setStartAnimation,
-    }: {
-      source: OverlaySource
-      setStartAnimation: (value: boolean) => void
-    }) => {
-      const { src, meta, height, width } = source?.sourceProps || {}
-      const iframeRef = React.useRef<HTMLIFrameElement>(null)
+    const IFrame = React.memo(
+      ({
+        source,
+        setStartAnimation,
+      }: {
+        source: OverlaySource
+        setStartAnimation: (value: boolean) => void
+      }) => {
+        const { src, meta, height, width } = source?.sourceProps || {}
+        const iframeRef = React.useRef<HTMLIFrameElement>(null)
 
-      useEffect(() => {
-        if (iframeRef.current) {
-          iframeRef.current.style.removeProperty('transformOrigin')
-          iframeRef.current.style.removeProperty('transform')
-        }
-      }, [src])
-
-      const resizeIframe = () => {
-        if (iframeRef.current) {
-          const project = getProject(CoreContext.state.activeProjectId)
-          const root = project.compositor.getRoot()
-          const { x: rootWidth, y: rootHeight } = root.props.size
-          let iframeWidth = iframeRef.current.clientWidth
-          let iframeHeight = iframeRef.current.clientHeight
-
-          let scale
-
-          if (iframeWidth && iframeHeight) {
-            scale = Math.min(rootWidth / iframeWidth, rootHeight / iframeHeight)
-          } else {
-            // It's possible the container will have no size defined (width/height=0)
-            scale = 1
+        useEffect(() => {
+          if (iframeRef.current) {
+            iframeRef.current.style.removeProperty('transformOrigin')
+            iframeRef.current.style.removeProperty('transform')
           }
+        }, [src])
 
-          iframeRef.current.style.willChange = `transform`
-          // @ts-ignore
-          iframeRef.current.style.transformOrigin = '0 0'
-          iframeRef.current.style.transform = `scale(${scale}) translateZ(0)`
-          setStartAnimation(true)
-        }
-      }
-      return (
-        <React.Fragment>
-          <Iframe
-            key={source.id}
-            url={src}
-            frameBorder={0}
-            iframeRef={iframeRef}
-            height={height}
-            width={width}
-            onLoad={resizeIframe}
-            styles={{ ...meta?.style }}
-          />
-        </React.Fragment>
-      )
-    })
+        const resizeIframe = () => {
+          if (iframeRef.current) {
+            const project = getProject(CoreContext.state.activeProjectId)
+            const root = project.compositor.getRoot()
+            const { x: rootWidth, y: rootHeight } = root.props.size
+            let iframeWidth = iframeRef.current.clientWidth
+            let iframeHeight = iframeRef.current.clientHeight
 
-    const Video = React.memo(({
-      source,
-      setStartAnimation,
-    }: {
-      source: OverlaySource
-      setStartAnimation: (value: boolean) => void
-    }) => {
-      const { src, type, meta, loop } = source?.sourceProps || {}
-      const { id, sourceType } = source || {}
-      const [refId, setRefId] = React.useState(null)
-      const videoRef = React.useRef<HTMLVideoElement>(null)
-      console.log('Updated current time', videoRef?.current?.currentTime)
+            let scale
 
-      /* A callback function that is called when the video element is created. */
-      const handleRect = React.useCallback((node: HTMLVideoElement) => {
-        videoRef.current = node
-        setRefId(node ? node.id : null)
-      }, [])
-
-      /* A callback function that is called when the video element is loaded. */
-      const onLoadedData = React.useCallback(() => {
-        if (videoRef?.current) {
-          videoRef.current!.play().catch(() => {
-            videoRef.current.muted = true
-            videoRef.current?.play()
-          })
-        }
-      }, [src])
-
-      /* A callback function that is called when the video playback ended. */
-      const onEnded = React.useCallback(() => {
-        if (interval) {
-          clearInterval(interval)
-        }
-        if (hasPermission(role, Permission.UpdateProject)) {
-          trigger('VideoEnded', { id: id, category: type })
-        }
-      }, [src])
-
-      /* Checking if the video is playing and if the user has permission to manage self and if the guest is
-      the same as the room participant id. If all of these are true, then it sets the current time of the
-      video to the meta time. */
-      React.useEffect(() => {
-        if (meta && videoRef?.current && refId) {
-          if (hasPermission(role, Permission.ManageSelf)) {
-            if (meta?.time) {
-              videoRef.current.currentTime = Number(meta?.time)
+            if (iframeWidth && iframeHeight) {
+              scale = Math.min(
+                rootWidth / iframeWidth,
+                rootHeight / iframeHeight,
+              )
+            } else {
+              // It's possible the container will have no size defined (width/height=0)
+              scale = 1
             }
+
+            iframeRef.current.style.willChange = `transform`
+            // @ts-ignore
+            iframeRef.current.style.transformOrigin = '0 0'
+            iframeRef.current.style.transform = `scale(${scale}) translateZ(0)`
+            setStartAnimation(true)
           }
         }
-      }, [meta?.time, refId])
+        return (
+          <React.Fragment>
+            <Iframe
+              key={source.id}
+              url={src}
+              frameBorder={0}
+              iframeRef={iframeRef}
+              height={height}
+              width={width}
+              onLoad={resizeIframe}
+              styles={{ ...meta?.style }}
+            />
+          </React.Fragment>
+        )
+      },
+    )
 
-      /* This is a React hook that is called when the component is unmounted. It clears the interval. */
-      React.useEffect(() => {
-        return () => {
-          if (interval) {
-            clearInterval(interval)
-          }
-        }
-      }, [id])
+    const Video = React.memo(
+      ({
+        source,
+        setStartAnimation,
+      }: {
+        source: OverlaySource
+        setStartAnimation: (value: boolean) => void
+      }) => {
+        const { src, type, meta, loop } = source?.sourceProps || {}
+        const { id, sourceType } = source || {}
+        const [refId, setRefId] = React.useState(null)
+        const videoRef = React.useRef<HTMLVideoElement>(null)
+        console.log('Updated current time', videoRef?.current?.currentTime)
 
-      /* This is a React hook that is called when the component is mounted and when the refId changes. */
-      React.useEffect(() => {
-        if (!refId) {
-          if (interval) {
-            clearInterval(interval)
-          }
-        } else {
-          if (videoRef.current) {
-            videoRef.current!.src = src
+        /* A callback function that is called when the video element is created. */
+        const handleRect = React.useCallback((node: HTMLVideoElement) => {
+          videoRef.current = node
+          setRefId(node ? node.id : null)
+        }, [])
+
+        /* A callback function that is called when the video element is loaded. */
+        const onLoadedData = React.useCallback(() => {
+          if (videoRef?.current) {
             videoRef.current!.play().catch(() => {
               videoRef.current.muted = true
-              videoRef.current.play()
+              videoRef.current?.play()
             })
-            if (hasPermission(role, Permission.UpdateProject)) {
-              interval = setInterval(() => {
-                if (videoRef.current.duration) {
-                  const timePending =
-                    videoRef.current.duration - videoRef.current.currentTime
-                  trigger('VideoTimeUpdate', {
-                    category: sourceType,
-                    id: id,
-                    time: Math.floor(timePending),
-                  })
-                }
-              }, 1000)
+          }
+        }, [src])
+
+        /* A callback function that is called when the video playback ended. */
+        const onEnded = React.useCallback(() => {
+          if (interval) {
+            clearInterval(interval)
+          }
+          if (hasPermission(role, Permission.UpdateProject)) {
+            trigger('VideoEnded', { id: id, category: type })
+          }
+        }, [src])
+
+        /* Checking if the video is playing and if the user has permission to manage self and if the guest is
+      the same as the room participant id. If all of these are true, then it sets the current time of the
+      video to the meta time. */
+        React.useEffect(() => {
+          if (meta && videoRef?.current && refId) {
+            if (hasPermission(role, Permission.ManageSelf)) {
+              if (meta?.time) {
+                videoRef.current.currentTime = Number(meta?.time)
+              }
             }
           }
-        }
-      }, [refId])
+        }, [meta?.time, refId])
 
-      return (
-        <React.Fragment key={id}>
-          {src && (
-            <video
-              loop={loop}
-              id={id}
-              ref={handleRect}
-              style={{ ...sourceProps.meta.style, ...meta.style }}
-              onLoadedData={onLoadedData}
-              onEnded={onEnded}
-              onCanPlay={() => setStartAnimation(true)}
-            />
-          )}
-        </React.Fragment>
-      )
-    })
+        /* This is a React hook that is called when the component is unmounted. It clears the interval. */
+        React.useEffect(() => {
+          return () => {
+            if (interval) {
+              clearInterval(interval)
+            }
+          }
+        }, [id])
 
-    const Image = React.memo(({
-      source,
-      setStartAnimation,
-    }: {
-      source: OverlaySource
-      setStartAnimation: (value: boolean) => void
-    }) => {
-      const { src, meta } = source?.sourceProps || {}
-      const { id } = source || {}
+        /* This is a React hook that is called when the component is mounted and when the refId changes. */
+        React.useEffect(() => {
+          if (!refId) {
+            if (interval) {
+              clearInterval(interval)
+            }
+          } else {
+            if (videoRef.current) {
+              videoRef.current!.src = src
+              videoRef.current!.play().catch(() => {
+                videoRef.current.muted = true
+                videoRef.current.play()
+              })
+              if (hasPermission(role, Permission.UpdateProject)) {
+                interval = setInterval(() => {
+                  if (videoRef.current.duration) {
+                    const timePending =
+                      videoRef.current.duration - videoRef.current.currentTime
+                    trigger('VideoTimeUpdate', {
+                      category: sourceType,
+                      id: id,
+                      time: Math.floor(timePending),
+                    })
+                  }
+                }, 1000)
+              }
+            }
+          }
+        }, [refId])
 
-      return (
-        <React.Fragment key={id}>
-          {src && (
-            <img
-              style={{ ...sourceProps.meta.style, ...meta.style }}
-              src={src}
-              onLoad={() => setStartAnimation(true)}
-            />
-          )}
-        </React.Fragment>
-      )
-    })
+        return (
+          <React.Fragment key={id}>
+            {src && (
+              <video
+                loop={loop}
+                id={id}
+                ref={handleRect}
+                style={{ ...sourceProps.meta.style, ...meta.style }}
+                onLoadedData={onLoadedData}
+                onEnded={onEnded}
+                onCanPlay={() => setStartAnimation(true)}
+              />
+            )}
+          </React.Fragment>
+        )
+      },
+    )
+
+    const Image = React.memo(
+      ({
+        source,
+        setStartAnimation,
+      }: {
+        source: OverlaySource
+        setStartAnimation: (value: boolean) => void
+      }) => {
+        const { src, meta } = source?.sourceProps || {}
+        const { id } = source || {}
+
+        return (
+          <React.Fragment key={id}>
+            {src && (
+              <img
+                style={{ ...sourceProps.meta.style, ...meta.style }}
+                src={src}
+                onLoad={() => setStartAnimation(true)}
+              />
+            )}
+          </React.Fragment>
+        )
+      },
+    )
 
     const Overlay = React.memo(({ source }: { source: OverlaySource }) => {
       const { type } = source?.sourceProps || {}
@@ -246,7 +255,11 @@ export const Overlay = {
           duration={400}
         >
           <div
-            style={{ opacity: startAnimation ? 1 : 0 }}
+            style={{
+              opacity: startAnimation ? 1 : 0,
+              height: '100%',
+              width: '100%',
+            }}
             className={`overlayContainer overlay-transition`}
           >
             {id && type === 'image' && (
